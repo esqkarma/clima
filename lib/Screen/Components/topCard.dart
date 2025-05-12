760bded575f193a2fe04469e496b10f51feee43e
vipinKarma <vipinkarma5@gmail.com>
2025-05-12 23:28:28 +0530

    design debug

--- lib/Screen/Components/topCard.dart
+++ lib/Screen/Components/topCard.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class TopCard extends StatelessWidget {
-  bool isDetail;
-  TopCard({required this.isDetail, super.key});
+  final bool isDetail;
+  const TopCard({required this.isDetail, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +20,7 @@ class TopCard extends StatelessWidget {
 
     return Center(
       child: AnimatedContainer(
-        height: isDetail ? height * 0.42 : height * 0.22,
+        height: isDetail ? height * 0.38 : height * 0.22,
         width: width * 0.92,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(15),
@@ -29,29 +29,31 @@ class TopCard extends StatelessWidget {
         duration: Duration(milliseconds: 600),
         curve: Curves.easeInOutSine,
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                height: isDetail ? height * 0.20 : height * 0.11,
-                width: width * 0.8188,
-                child: Image.asset(
-                  iconsPath[condi] ?? 'assets/weatherCondition/rocket.png',
-                  filterQuality: FilterQuality.high,
-                ),
-              ),
-              SizedBox(
-                height: isDetail ? height * 0.13 : height * 0.065,
-                width: width * 0.50,
-                child: Center(
-                  child: Text(
-                    temp != null ? '${temp}\u00B0C' : 'N/A',
-                    style: TextStyle(fontSize: isDetail ? 70 : 30, color: Colors.white),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  height: isDetail ? height * 0.20 : height * 0.11,
+                  width: width * 0.8188,
+                  child: Image.asset(
+                    iconsPath[condi] ?? 'assets/weatherCondition/rocket.png',
+                    filterQuality: FilterQuality.high,
                   ),
                 ),
-              ),
-              Text(data?.weatherCondition ?? 'N/A', style: TextStyle(color: Colors.grey[400])),
-            ],
+                SizedBox(
+                  height: isDetail ? height * 0.13 : height * 0.065,
+                  width: width * 0.50,
+                  child: Center(
+                    child: Text(
+                      temp != null ? '${temp}\u00B0C' : 'N/A',
+                      style: TextStyle(fontSize: isDetail ? 70 : 30, color: Colors.white),
+                    ),
+                  ),
+                ),
+                Text(data?.weatherCondition ?? 'N/A', style: TextStyle(color: Colors.grey[400])),
+              ],
+            ),
           ),
         ),
       )
